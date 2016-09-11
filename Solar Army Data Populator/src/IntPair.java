c2682b1e37f27e0010637f0d0e66fa8b6e40b37e
Benjamin Wilfong <ben.wilfong@gmail.com>
2016-09-11 00:36:05 -0400

    Made possible to use object as comparison for key value in hash table

--- Solar Army Data Populator/src/IntPair.java
+++ Solar Army Data Populator/src/IntPair.java
@@ -4,4 +4,19 @@ class IntPair {
         this.p1 = p1;
         this.p2 = p2;
     }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof IntPair)) return false;
+        IntPair key = (IntPair) o;
+        return p1 == key.p1 && p2 == key.p2;
+    }
+    
+    @Override
+    public int hashCode() {
+        int result = p1;
+        result = 31 * result + p2;
+        return result;
+    }
 }
