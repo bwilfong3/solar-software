de8d3dd94d7ec9b39dda22a2e5e4d67a51bc9925
Benjamin Wilfong <ben.wilfong@gmail.com>
2016-09-15 04:14:16 -0400

    Added query to check if template file is in database. If it is, it checks to see what data files are associated with the template file chosen

--- Solar Army Data Populator/src/SolarArmyDBC.java
+++ Solar Army Data Populator/src/SolarArmyDBC.java
@@ -1,5 +1,10 @@
 // This class will be used to execute some of the specific queries needed for the program
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.util.Queue;
+
 public class SolarArmyDBC extends DatabaseConnector{
 	final String templateExistsQuery = "SELECT * FROM template WHERE t_file_name = ?";
 		// t_file_name
@@ -18,4 +23,55 @@ public class SolarArmyDBC extends DatabaseConnector{
 	
 	final String associateDataFile = "INSERT INTO associated_with_data VALUES(?,?)";
 		// t_file_name, uid
+	
+	Queue<PreparedStatement> q;
+	
+	public boolean checkIfTemplateExists(String tFileName){
+		try{
+		String queryString = "SELECT * FROM template WHERE t_file_name = ?;";
+		PreparedStatement ps = connection.prepareStatement(queryString);
+		ps.setString(1,  tFileName);
+		
+		ResultSet resultSet = ps.executeQuery();// query database
+		 if(!resultSet.next())
+		 {
+			 System.out.println( tFileName + " does not yet exist in the database.");
+			 resultSet.close();
+			 return false;
+		 }
+		 else
+		 {
+			System.out.printf( "Template file already exists in database.");
+			resultSet.close();
+			getDataFiles(tFileName);
+			return true;
+		 }
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	private void getDataFiles(String tFileName){
+		try{
+		String queryString = "SELECT d_file_name FROM data_file WHERE uid = " +
+					         "(SELECT uid FROM associated_with_data WHERE t_file_name = ?);";
+		PreparedStatement ps = connection.prepareStatement(queryString);
+		ps.setString(1,  tFileName);
+		
+		ResultSet resultSet = ps.executeQuery();
+		
+		if(!resultSet.next())
+			System.out.println("No data files yet associated with " + tFileName);
+		else{
+			do{
+				System.out.println(resultSet.getString("d_file_name"));
+			}while(resultSet.next());
+		}
+		}catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
